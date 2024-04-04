71f7d4693b4436d1b31e1dd5b1f25f5ea881c261
PHR <PHR@DESKTOP-LVCEFG0>
2024-04-04 16:28:11 +0900

    Study Start!

---
+++ src/main/java/com/hrpark/tobyStudy/TobyStudyApplication.java
@@ -0,0 +1,13 @@
+package com.hrpark.tobyStudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TobyStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TobyStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hrpark/tobyStudy/TobyStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hrpark.tobyStudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TobyStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
