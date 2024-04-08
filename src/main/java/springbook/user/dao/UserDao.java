f6b9db8f6b685ec92dcabb563d3e10bbe560398c
PHR <PHR@DESKTOP-LVCEFG0>
2024-04-08 16:45:48 +0900

    DB설정 추가 및 INSERT,SELECT,DELETE 로직 추가

--- src/main/java/springbook/user/dao/UserDao.java
+++ src/main/java/springbook/user/dao/UserDao.java
@@ -12,10 +12,10 @@ public class UserDao {
 
     public void add(User user) throws ClassNotFoundException, SQLException {
     	
-    	Class.forName("com.mysql.jdbc.Driver");
-    	Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/springbook", "spring", "book");
+    	Class.forName("com.mysql.cj.jdbc.Driver");
+    	Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/toby_spring", "root", "0389");
     	
-        String sql = "insert into users(ID, PASSWORD, USER_NAME) values(?,?,?)";
+        String sql = "insert into User(ID, PASSWORD, USER_NAME) values(?,?,?)";
 
         PreparedStatement preparedStatement = connection.prepareStatement(sql);
         preparedStatement.setString(1, user.getId());
@@ -25,14 +25,28 @@ public class UserDao {
         preparedStatement.executeUpdate();
 
     }
+    
+    public void delete(User user) throws ClassNotFoundException, SQLException {
+    	
+    	Class.forName("com.mysql.cj.jdbc.Driver");
+    	Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/toby_spring", "root", "0389");
+    	
+        String sql = "delete from User where ID = ?";
+
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        preparedStatement.setString(1, user.getId());
+
+        preparedStatement.executeUpdate();
+
+    }    
 
     public User get(String id) throws ClassNotFoundException, SQLException {
-    	
 
-    	Class.forName("com.mysql.jdbc.Driver");
-    	Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/springbook", "spring", "book");  	
+    	Class.forName("com.mysql.cj.jdbc.Driver");
+    	Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/toby_spring", "root", "0389");  	
     	
-        String sql = "select id, name, password from users where id = ?";
+        String sql = "select ID, PASSWORD, USER_NAME from User where id = ?";
+        
         PreparedStatement preparedStatement = connection.prepareStatement(sql);
         preparedStatement.setString(1, id);
         
@@ -40,9 +54,9 @@ public class UserDao {
         rs.next();
         
         User user = new User();
-        user.setId(rs.getString("id"));
-        user.setPassword(rs.getString("password"));
-        user.setuserName(rs.getString("userName"));
+        user.setId(rs.getString("ID"));
+        user.setPassword(rs.getString("PASSWORD"));
+        user.setuserName(rs.getString("USER_NAME"));
 
         rs.close();
         preparedStatement.close();
